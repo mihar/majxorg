02cb12a7bffe7d00199dc23ed40f1a9d480476c5
Miha Rebernik <rasmajx@gmail.com>
2011-09-21 22:07:41 -0700

    Adding source blog of post to post listing.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def format_source(source)
+    case source
+    when 'tumblr'
+      link_to "break the bit", "http://breakthebit.org", :title => "My tech blog &ldquo;break the bit&rdquo; (http://breakthebit.org)".html_safe
+    when 'posterous'
+      link_to "break the beat", "http://breakthebeat.org", :title => "My life blog &ldquo;break the beat&rdquo; (http://breakthebeat.org)".html_safe
+    end
+  end
 end
