1c618fe455b2ff37a682b002a9afcaed98ec1c70
Miha Rebernik <rasmajx@gmail.com>
2011-09-21 23:23:22 -0700

    Nitpicking.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,11 +4,11 @@ var Geolocation = {
   // Distance in kilometers between two points using the Haversine algo.
   haversine: function(p1, p2) {
     var R = 6371 // Earth's mean radius in km.
-    var dLat  = this.rad(p2.lat - p1.lat)
-    var dLong = this.rad(p2.lng - p1.lng)
+    var dLat  = this.rad(p2.latitude - p1.latitude)
+    var dLong = this.rad(p2.longitude - p1.longitude)
 
     var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) * Math.sin(dLong/2) * Math.sin(dLong/2)
+            Math.cos(this.rad(p1.latitude)) * Math.cos(this.rad(p2.latitude)) * Math.sin(dLong/2) * Math.sin(dLong/2)
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
     var d = R * c
 
@@ -18,7 +18,7 @@ var Geolocation = {
   // Distance between me and the passed position.
   distance_from: function(position) {
     Geolocation.display_location()
-    var distance = Geolocation.haversine({lat: position.coords.latitude, lng: position.coords.longitude}, current_location)
+    var distance = Geolocation.haversine(position.coords, current_location)
 
     if ( distance && distance > 0 ) $("#distance").text(distance + " km")
     else $("#user_distance").html("<strong>You're close!</strong> Watch my toes!")
