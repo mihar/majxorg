2a35a08a62a877b11ac71549d98b0bd66d52c02b
Miha Rebernik <rasmajx@gmail.com>
2011-09-21 22:07:51 -0700

    Pimping the geolocation object.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,36 +1,44 @@
-rad = function(x) {return x*Math.PI/180;}
+var Geolocation = {
+  rad: function(x) { return x * Math.PI / 180 },
+  
+  // Distance in kilometers between two points using the Haversine algo.
+  haversine: function(p1, p2) {
+    var R = 6371 // Earth's mean radius in km.
+    var dLat  = this.rad(p2.lat - p1.lat)
+    var dLong = this.rad(p2.lng - p1.lng)
 
-distHaversine = function(p1, p2) {
-  var R = 6371; // earth's mean radius in km
-  var dLat  = rad(p2.lat - p1.lat);
-  var dLong = rad(p2.lng - p1.lng);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(this.rad(p1.lat)) * Math.cos(this.rad(p2.lat)) * Math.sin(dLong/2) * Math.sin(dLong/2)
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    var d = R * c
 
-  var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
-          Math.cos(rad(p1.lat)) * Math.cos(rad(p2.lat)) * Math.sin(dLong/2) * Math.sin(dLong/2);
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c;
+    return Math.round(d)
+  },
+  
+  // Distance between me and the passed position.
+  distance_from: function(position) {
+    Geolocation.display_location()
+    var distance = Geolocation.haversine({lat: position.coords.latitude, lng: position.coords.longitude}, current_location)
 
-  return Math.round(d);
-}
-
-calculateDistance = function(position) {
-  displayLocation()
-  $("#distance").text(distHaversine({lat: position.coords.latitude, lng: position.coords.longitude}, current_location) + " km")
-}
-
-displayLocation = function() {
-  $("#user_distance").show()
-  $("#geolocating").hide()
+    if ( distance && distance > 0 ) $("#distance").text(distance + " km")
+    else $("#user_distance").html("<strong>You're close!</strong> Watch my toes!")
+  },
+  
+  // Hide spinner and show location.
+  display_location: function() {
+    $("#user_distance").show()
+    $("#geolocating").hide()
+  }
 }
 
 $(function() {
+  
   // Geolocation.
-  $("#user_distance").hide()
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(calculateDistance, displayLocation)
-    navigator.geolocation.watchPosition(calculateDistance)
+    navigator.geolocation.getCurrentPosition(Geolocation.distance_from, Geolocation.display_location)
+    navigator.geolocation.watchPosition(Geolocation.distance_from)
   } else {
-    displayLocation()
+    Geolocation.display_location()
   }
   
   function slide_down_location() {
@@ -46,20 +54,20 @@ $(function() {
     return false
   })
   
-  var fixed = false;
+  var fixed = false
 
   $(document).scroll(function() {
     if( $(this).scrollTop() >= 75 ) {
       if( !fixed ) {
-        fixed = true;
-        $('#side').css({position: 'fixed', top: "25px"});
+        fixed = true
+        $('#side').css({position: 'fixed', top: "25px"})
       }                                          
     } else {
       if( fixed ) {
-        fixed = false;
-        $('#side').css({position:'absolute', top: "100px"});
+        fixed = false
+        $('#side').css({position:'absolute', top: "100px"})
       }
     }
-  });
+  })
   
 })
