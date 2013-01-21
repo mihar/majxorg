350fcc397a8eca582d3039137a0bcd51fc24637a
Miha Rebernik <miha@rebernik.info>
2013-01-20 21:03:50 -0800

    FOG.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,7 +41,7 @@ V8::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server
-  config.action_controller.asset_host = "http://majxorg.s3.amazonaws.com"
+  config.action_controller.asset_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w( modernizr.js dd_belatedpng.js )
